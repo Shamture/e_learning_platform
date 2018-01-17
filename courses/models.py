2169df485139f136983a3624f94932579208c0d8
Aaron Zhao <rudy710@qq.com>
2018-01-18 00:40:03 +0800

    add field student to course

--- courses/models.py
+++ courses/models.py
@@ -20,6 +20,7 @@ class Subject(models.Model):
 
 class Course(models.Model):
     owner = models.ForeignKey(User, related_name='courses_created', on_delete=models.CASCADE)
+    student = models.ManyToManyField(User, related_name='course_joined', blank=True)
     subject = models.ForeignKey(Subject, related_name='courses', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)

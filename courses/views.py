b61a6aa4242433e04c3d766ca6ca2fa8388c207c
Aaron Zhao <rudy710@qq.com>
2018-01-19 22:20:42 +0800

    add student_enroll, student_courst_list, student_course_detail views to students app

--- courses/views.py
+++ courses/views.py
@@ -11,6 +11,7 @@ from django.forms.models import modelform_factory
 from django.apps import apps
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from students.forms import CourseEnrollForm
 
 # Create your views here.
 
@@ -52,6 +53,11 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CourseDetailView, self).get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(initial={'course': self.object})
+        return context
 
 class CourseCreateView(PermissionRequiredMixin, OwnerCourseEditMixin, CreateView):
     permission_required = 'courses.add_course'
--- students/views.py
+++ students/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, FormView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from students.forms import CourseEnrollForm
+from courses.models import Course
 
 # Create your views here.
 
@@ -19,3 +24,39 @@ class StudentRegistrationView(CreateView):
                             password=cd['password1'])
         login(self.request, user)
         return result
+
+class StudentEnrollCourseView(LoginRequiredMixin, FormView):
+    course = None
+    form_class = CourseEnrollForm
+
+    def form_valid(self, form):
+        self.course = form.cleaned_data['course']
+        self.course.students.add(self.request.user)
+        return super(StudentEnrollCourseView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('students:student_course_detail', args=[self.course.id])
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super(StudentCourseListView, self).get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+class StudentCourseDetailView(LoginRequiredMixin, DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_context_data(self, **kwargs):
+        print(kwargs)
+        context = super(StudentCourseDetailView, self).get_context_data(**kwargs)
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            # get current module
+            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+        else:
+            # get first module
+            context['module'] = course.modules.all()[0]
+        return context
